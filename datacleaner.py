3d5c6a5f6bdbeefc226f3ca65c820bd51a904869
Shaivi <shaivigandhi@yahoo.com>
2024-05-05 00:06:14 -0300

    modified params in clean_audio

--- datacleaner.py
+++ datacleaner.py
@@ -7,18 +7,23 @@ import noisereduce as nr
 
 dataset = load_dataset("DORI-SRKW/DORI-Orcasound")
 
-input_dir = "input_audio"
+input_dir = "downloaded_files"
 output_dir = "cleaned_audio"
 
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 
-def clean_audio(input_file, outputfile):
+def clean_audio(input_file, output_file):
+    # Load the audio file
     y, sr = librosa.load(input_file, sr=None)
-    y_resample = librosa.resample(y, sr, 16000)
-    y_deNoised = nr.reduce_noise(y_resample)
-    sf.write(outputfile, y_deNoised, samplerate=16000)
+    # Resample the audio to 16000 Hz
+    y_resample = librosa.resample(y, orig_sr=sr, target_sr=16000)
+    # Reduce noise from the resampled audio
+    y_deNoised = nr.reduce_noise(y=y_resample, sr=16000)
+    # Write the cleaned audio to a file
+    sf.write(output_file, y_deNoised, samplerate=16000)
+
 
 
 for filename in os.listdir(input_dir):
